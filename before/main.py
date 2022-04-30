e41a9677d31c33fda8e0fc7b9c36be288c2fd8a7
marktodisco <marktodisco@gmail.com>
2022-04-30 13:08:25 -0400

    removed old class

--- before/main.py
+++ before/main.py
@@ -68,21 +68,5 @@ def select_all_years(
     return sorted(new_years), n_clicks
 
 
-class SelectAllYears:
-    outputs = [Output("year-dropdown", "value"), Output("select-all-year-button-clicks", "data")]
-    inputs = [
-        Input("year-dropdown", "value"),
-        Input("select-all-year-button", "n_clicks"),
-        Input("select-all-year-button-clicks", "data"),
-    ]
-
-    def func(
-        self, years: list[int], n_clicks: int, previous_n_clicks: int
-    ) -> tuple[list[int], int]:
-        clicked = n_clicks <= previous_n_clicks
-        new_years = years if clicked else list(transactions.dropna()["Year"].unique())
-        return sorted(new_years), n_clicks
-
-
 if __name__ == "__main__":
     app.run_server(debug=True)

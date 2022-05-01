f6e837c330b96fdef69941c44caf6fe7cf0f99ec
marktodisco <marktodisco@gmail.com>
2022-05-01 09:25:12 -0400

    cleaned up file

--- before/src/app_utils.py
+++ before/src/app_utils.py
@@ -1,7 +1,6 @@
-from string import ascii_letters
 from random import choices
+from string import ascii_letters
 
 
 def generate_random_id(length: int = 32) -> str:
     return "".join(choices(list(ascii_letters), k=length))
-

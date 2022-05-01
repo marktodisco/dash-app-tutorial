476032c6b4ce8f059767eb751ef01fc6bed672db
marktodisco <marktodisco@gmail.com>
2022-05-01 09:26:47 -0400

    required transactions to be an argument to helper functions in default_options module

--- before/src/default_options.py
+++ before/src/default_options.py
@@ -1,38 +1,34 @@
 from datetime import datetime
-from typing import List, Dict, Union
-
-from src.data import load_transaction_data
 
 import pandas as pd
 
+from src.typing.classes import BudgetDataFrame
 
-def get_year_options() -> List[Dict[str, str]]:
-    transactions = load_transaction_data()
+
+def get_year_options(transactions: BudgetDataFrame) -> list[dict[str, str]]:
     return [{"label": i, "value": i} for i in transactions["Year"].unique()]
 
 
-def get_month_options() -> List[Dict[str, str]]:
-    transactions = load_transaction_data()
+def get_month_options(transactions: BudgetDataFrame) -> list[dict[str, str]]:
     return [{"label": i, "value": i} for i in transactions["Month"].unique()]
 
 
-def get_category_options() -> List[Dict[str, str]]:
-    categories = get_category_values()
+def get_category_options(transactions: BudgetDataFrame) -> list[dict[str, str]]:
+    categories = get_category_values(transactions)
     base_options = [{"label": i, "value": i} for i in categories]
     filtered_options = filter(lambda option: not pd.isna(option["label"]), base_options)
     final_options = sorted(filtered_options, key=lambda x: x["label"])
     return final_options
 
 
-def get_category_values() -> List[str]:
-    transactions = load_transaction_data()
+def get_category_values(transactions: BudgetDataFrame) -> list[str]:
     categories = transactions["Category"].unique()
     return sorted(filter(lambda category: not pd.isna(category), categories))
 
 
-def get_year_values() -> List[Union[str, int]]:
+def get_year_values() -> list[str | int]:
     return [datetime.now().year]
 
 
-def get_month_values() -> List[Union[str, int]]:
+def get_month_values() -> list[str | int]:
     return [datetime.now().strftime("%m-%b")]

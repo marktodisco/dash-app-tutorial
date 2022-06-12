7ca5bd1861c33274c97e407603b73bf6866342e3
marktodisco <marktodisco@gmail.com>
2022-06-12 15:40:45 -0400

    removed hardcoded amount and category columns

--- src/callbacks/update_pie_chart.py
+++ src/callbacks/update_pie_chart.py
@@ -15,7 +15,11 @@ def register(app: dash.Dash) -> None:
     )
     def _update_pie_chart(pivot_table_records: list[dict[str, float]]) -> dcc.Graph:
         pivot_table = pd.DataFrame(pivot_table_records)
-        pie = go.Pie(labels=pivot_table["category"], values=pivot_table["amount"], hole=0.5)
+        pie = go.Pie(
+            labels=pivot_table.loc[:, src.schema.RawTransactionsSchema.category],
+            values=pivot_table.loc[:, src.schema.RawTransactionsSchema.amount],
+            hole=0.5,
+        )
         fig = go.Figure(data=[pie])
         fig.update_layout(margin=dict(t=40, b=0, l=0, r=0))
         fig.update_traces(hovertemplate="%{label}<br>$%{value:.2f}<extra></extra>")
